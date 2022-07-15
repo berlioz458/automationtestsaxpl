07a69281daca0b64da95c621fb83cae3d05b6cfb
shulinina.e <test-2@prodv.net>
2022-07-15 14:50:43 +0700

    - work regMan smoke

--- src/test/java/clientservice/registrationmanager/models/PrincipalAvailability.java
+++ src/test/java/clientservice/registrationmanager/models/PrincipalAvailability.java
@@ -2,6 +2,7 @@ package clientservice.registrationmanager.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,5 +25,13 @@ public class PrincipalAvailability {
     @JsonProperty("conflictPrincipal")
     private String conflictPrincipal;
     @JsonProperty("conflictedEntities")
-    private List<String> conflictedEntities;
+    private List<ConflictedEntity> conflictedEntities;
+
+    @Data @NoArgsConstructor @AllArgsConstructor
+    @JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
+    @JsonTypeName("ConflictedEntity")
+    public static class ConflictedEntity {
+        private Integer id;
+        private String type;
+    }
 }
