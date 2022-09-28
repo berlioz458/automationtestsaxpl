e12b4040d632395f1e6a683d01f33028033a06be
shulinina.e <test-2@prodv.net>
2022-09-28 20:22:04 +0700

    - модели для OS

--- src/test/java/orderservice/models/ProvisionPlan.java
+++ src/test/java/orderservice/models/ProvisionPlan.java
@@ -1,4 +1,42 @@
 package orderservice.models;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+//@AllArgsConstructor
+@JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
 public class ProvisionPlan {
+    private Integer id = 4681767;
+    private String actualityDate = "2022-12-31T08:23:03.820+0000";
+    private Boolean aviaDelivery = false;
+    private String brandOriginal = "TRW";
+    private Integer deliveryCost = 0;
+    private String deliveryDateMax , deliveryDateMin = "2022-12-31T08:23:03.820+0000";
+    private Integer deliveryIntervalMax, deliveryIntervalMin = 88616;
+    private String descrOriginal = "GDB3582_колодки дисковые п.! \\\\ KIA Optima 10>";
+    private Integer destinationRoutingPointId = 10456;
+    private String destinationRoutingPointName =  "Куликовская ( Москва Агентство Авто 3Н) СП";
+    private Integer detail = 113687910;
+    private Integer loadingId = 6323303;
+    private String maxReturnDate = "2023-01-14T08:23:03.820+0000";
+    private double minPrice = 1947.38;
+    private Integer multiplicityPartyOriginal = 1;
+    private String oemOriginal = "GDB3582";
+    private String offerId = "PIN:GDB3582:BRAND:TRW:VKORG:4000:KUNNR_RG:43275565:KEYZAK:MOV0000019:";
+    private Integer offerLineId = 117727;
+    private String placementDateTime = "2022-10-05T08:30:00.000+0000";
+    private Integer pricelistAgentId = 10401;
+    private Integer requestAgentId = 10515;
+    private Integer requestCurrencyId = 643;
+    private Integer requestPriceGroupId = 10001;
+    private Boolean returnsAllowed = true;
+    private Integer sellerCurrencyId = 643;
+    private Integer sellerPrice = 1887;
+    private Integer sourcePriceListId = 13167;
+    private Integer supplierAgentId = 1242;
+    private String supplierAvailabilityDateTime = "2022-10-02T10:30:00.000+0000";
+    private String supplierKeyOriginal = "GDB3582_TRW";
 }
