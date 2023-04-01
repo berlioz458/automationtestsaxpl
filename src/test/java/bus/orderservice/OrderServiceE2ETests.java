5c4495ffbdf53541c6d0267273da4b492ab32513
berlioz458 <berlioz458@gmail.com>
2023-04-01 13:32:20 +0700

    Добавлен sleep 3 минуты, для "проверки обмена", заказ 1С-ка прожевала

--- src/test/java/bus/orderservice/OrderServiceE2ETests.java
+++ src/test/java/bus/orderservice/OrderServiceE2ETests.java
@@ -5,6 +5,7 @@ import io.qameta.allure.Description;
 import org.junit.jupiter.api.Test;
 
 import static bus.orderservice.utils.OrderServiceApiController.*;
+import static com.codeborne.selenide.Selenide.sleep;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderServiceE2ETests {
@@ -23,6 +24,9 @@ public class OrderServiceE2ETests {
         //2. получение информации о созданном заказе, проверим что вернулся именно тот заказ что мы создали
         Order infoOrder = getOrderContract(createOrder.getId());
         assertThat(createOrder.getId()).isEqualTo(infoOrder.getId());
+        sleep(180000); // дурная затея, но что делать...3 минуты должна укладываться
+        infoOrder = getOrderContract(createOrder.getId());
+        assertThat(infoOrder.getChangedByUser()).isEqualTo("ERP-auto3n");
         //3. переведем заказ в работу
         Order inWorkOrder = changeStatusForOrder(infoOrder, 6);
         assertThat(inWorkOrder.getStatus().getId()).isEqualTo(6);
