58a51277c0d0561902587c404c3703e715f579fd
Zarazaxx <meduza1995shtin@gmail.com>
2023-08-18 18:56:44 +0700

    https://dev.prodv.net/browse/TAXPL-237

--- src/test/java/integrationservice/model/AgentTag.java
+++ src/test/java/integrationservice/model/AgentTag.java
@@ -2,15 +2,12 @@ package integrationservice.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
-@NoArgsConstructor
-@AllArgsConstructor
 public class AgentTag {
     public Integer id;
     public String name;
